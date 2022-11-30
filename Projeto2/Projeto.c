8c3989c9d61bbda52404bd7a36ca2e8935f4bde1
Luiz Gustavo Dalmaz <luiz.dalmaz@ufpr.br>
2022-11-30 03:59:08 -0300

    [+] Updated Work

--- Projeto2/Projeto.c
+++ Projeto2/Projeto.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "filelib.h"
 
 /* Forma de chamada do algoritmo: */
 /* ./lattes -d <diretorio com os CVs> -c <arquivo com a lista Qualis Conf> -p <arquivo com a lista Qualis Periódicos> */
 
 void main (int argc, char *argv[]) 
 {
-    /* Testa o numero de entradas e retorna Erro caso argc != 7 */
+    /* Testa o numero de entradas para que seja compativel com o programa */
     if (argc != 7) {
         printf("ERRO: número de argumentos inválido.\n");
         printf("Forma de chamada: \n./lattes -d </dir com os CVs> -c <.txt Lista Qualis Conf> -p <.txt Lista Qualis Periódicos>");
         exit(1);
     }
-    
-    /* Analisa a entrada para relacionar os arquivos com suas variaveis */
-    /* Obedecendo a ordem "-comando" seguido do "arquivo correspondente" */
+    /* Analisa a entrada para relacionar os arquivos com suas variaveis
+     * Obedecendo a ordem "-comando" seguido do "arquivo correspondente" */
     char *diretorio, *arquivoConf, *arquivoPeriodicos;
     int i;
-    for (i = 0; i < argc; i++) {
+    for (i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-d") == 0) {
             diretorio = argv[i+1];
         }
@@ -29,30 +29,10 @@ void main (int argc, char *argv[])
             arquivoPeriodicos = argv[i+1];
         }
     }
-    /* Abre o arquivo de Qualis de Conferencias */
-    FILE *arqConf = fopen(arquivoConf, "r");
-    if (arqConf == NULL) {
-        printf("ERRO: Arquivo de Lista de Conferencias '%s' não encontrado.\n", arquivoConf);
+    if (diretorio && arquivoConf && arquivoPeriodicos == NULL) {
+        printf("ERRO: arquivo não encontrado.\n");
         exit(1);
     }
-    /* Abre o arquivo de Qualis de Periodicos */
-    FILE *arqPeriodicos = fopen(arquivoPeriodicos, "r");
-    if (arqPeriodicos == NULL) {
-        printf("ERRO: Arquivo de Lista de Periodicos '%s' não encontrado.\n", arquivoPeriodicos);
-        exit(1);
-    }
-    /* Abre o arquivo de Qualis de Periodicos */
-    FILE *arqDiretorio = fopen(diretorio, "r");
-    if (arqDiretorio == NULL) {
-        printf("ERRO: Diretorio '%s' não encontrado.\n", diretorio);
-        exit(1);
-    }
-    /* Le o arquivo indicado e retorna um ponteiro para o stream */
-    FILE *arquivo = fopen("curriculo.xml", "r");
-    if (arquivo == NULL)
-    {
-        printf("Erro ao abrir o arquivo");
-        exit(1);
-    }
+
 
 }
--- Projeto2/filelib.c
+++ Projeto2/filelib.c
@@ -3,46 +3,50 @@
 #include <string.h>
 #include "filelib.h"
 
-/*  */
-
-char* file_read(char *nome_arquivo, int *tamanho_arquivo)
+/* Abre o arquivo indicado e retorna o conteudo do mesmo em uma string 
+ * Deve ser feito o free do conteudo posteriormente */
+char* arq_leitura(char *nome_arquivo)
 {
     /* Abre o arquivo para leitura indicado e retorna um ponteiro para o stream */    
     FILE *arquivo = fopen(nome_arquivo, "r");
     if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo\n");
+        printf("ERRO: arquivo %s não encontrado\n", nome_arquivo);
         exit(1);
     }
+
+    /* Procura o final do arquivo para obter o tamanho total do conteudo 
+     * retorna o ponteiro ao inicio e aloca o espaco necessario */
+
     /* Chega ao final do arquivo */
     if (fseek(arquivo, 0, SEEK_END) != 0) {
-        printf("Erro ao chegar ao final do arquivo\n");
+        printf("ERRO ao chegar ao final do arquivo %s\n", nome_arquivo);
         exit(1);
     }
     /* Retorna o numero de bytes do arquivo */
-    *tamanho_arquivo = ftell(arquivo);
-    if (*tamanho_arquivo == -1L) {
-        printf("Erro ao retornar o tamanho do arquivo\n");
+    int tamanho_arquivo = ftell(arquivo);
+    if (tamanho_arquivo == -1L) {
+        printf("ERRO ao retornar o tamanho do arquivo %s\n", nome_arquivo);
         exit(1);
     }
     /* Retorna o ponteiro para o inicio do arquivo */
     if (fseek(arquivo, 0, SEEK_SET) != 0) {
-        printf("Erro ao retornar o ponteiro para o inicio do arquivo\n");
+        printf("ERRO ao retornar o ponteiro para o inicio do arquivo %s\n", nome_arquivo);
         exit(1);
     }
     /* Aloca memoria para o conteudo do arquivo */
-    char *conteudo_arquivo = (char *) malloc(*tamanho_arquivo);
+    char *conteudo_arquivo = (char *) malloc(tamanho_arquivo);
     if (conteudo_arquivo == NULL) {
-        printf("Erro ao alocar memoria para o conteudo do arquivo\n");
+        printf("ERRO ao alocar memoria para o conteudo do arquivo %s\n", nome_arquivo);
         exit(1);
     }
     /* Le o conteudo do arquivo na memoria */
-    if (fread(conteudo_arquivo, 1, *tamanho_arquivo, arquivo) != *tamanho_arquivo) {
-        printf("Erro ao ler o conteudo do arquivo\n");
+    if (fread(conteudo_arquivo, 1, tamanho_arquivo, arquivo) != tamanho_arquivo) {
+        printf("ERRO ao ler o conteudo do arquivo %s\n", nome_arquivo);
         exit(1);
     }
     /* Fecha o arquivo */
     if (fclose(arquivo) != 0) {
-        printf("Erro ao fechar o arquivo\n");
+        printf("ERRO ao fechar o arquivo\n");
         exit(1);
     }
     /* Retorna o conteudo do arquivo */
@@ -51,7 +55,7 @@ char* file_read(char *nome_arquivo, int *tamanho_arquivo)
 
 /* Realiza a busca de uma string e retorna o numero de ocorrencias da mesma 
  * Recebe a "Palavra" a ser procurada, o Local onde procurar e o tamanho deste local */
-int str_grep(char *conteudo, int tamanho, char *palavra)
+int str_busca(char *conteudo, int tamanho, char *palavra)
 {
     int ocorrencia = 0;
     char *posicao_ocorrencia = conteudo;
@@ -60,4 +64,12 @@ int str_grep(char *conteudo, int tamanho, char *palavra)
         ocorrencia++;
         posicao_ocorrencia += strlen(palavra);
     }
+    return ocorrencia;
+}
+
+/* Separa todos os periodicos ou revistas do curriculo
+ * Recebe o conteudo do arquivo e o tamanho do mesmo, e devolve a lista de periodicos */
+char* separa_periodicos(char *conteudo, int tamanho)
+{
+    /* Localiza os periodicos e os escreve em uma nova string */
 }
